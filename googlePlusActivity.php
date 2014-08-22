f3939985357921e78030d5ee1b8dec5f82fcb3b7
bigbluewp <bigbluewp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-22 21:39:50 +0000

    stable release googlePlusActivity.php
    git-svn-id: https://plugins.svn.wordpress.org/google-plus-activity/trunk@971068 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ googlePlusActivity.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * @package wp-content/plugins/google-plus-activity
+*/
+/*
+Plugin Name: Google Plus Activity
+Plugin URI: http://bigbluegroup.net/
+Description: Thanks for installing Google Plus Activity
+Version: 0.1
+Author: Big Blue Group
+Author URI: http://bigbluegroup.net/
+*/
+
+class GooglePlusActivity extends WP_Widget{
+    public function __construct() {
+	add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_styles_google_plus_activity' ) );
+        $params = array(
+            'description' => 'Thanks for installing Google Plus Activity.',
+            'name' => 'Google Plus Activity'
+        );
+        parent::__construct('GooglePlusActivity','',$params);
+    }
+    public function register_plugin_styles_google_plus_activity() {
+        wp_register_style( 'GooglePlusActivityStyle', plugins_url( 'google-plus-activity/assets/gplus.css' ) );
+        wp_enqueue_style( 'GooglePlusActivityStyle' );
+    }
+    
+    public function form($instance) {
+        extract($instance);
+        
+?>
+<p>
+    <label for="<?php echo $this->get_field_id('title');?>">Title : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('title');?>"
+	name="<?php echo $this->get_field_name('title');?>"
+        value="<?php echo !empty($title) ? $title : "Google Plus Activity"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('userid');?>">Google Plus User ID : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('userid');?>"
+	name="<?php echo $this->get_field_name('userid');?>"
+        value="<?php echo !empty($userid) ? $userid : "116899029375914044550"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('gplus_api');?>">Google Plus API : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('gplus_api');?>"
+	name="<?php echo $this->get_field_name('gplus_api');?>"
+        value="<?php echo !empty($gplus_api) ? $gplus_api : "AIzaSyCPtQ1XMWetDkVw5XuzNnVXpf8ezDtH9So"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('count');?>">Max number of Google Plus Posts : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('count');?>"
+	name="<?php echo $this->get_field_name('count');?>"
+        value="<?php echo !empty($count) ? $count : "5"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('maxwidth');?>">Maximum Width : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('maxwidth');?>"
+	name="<?php echo $this->get_field_name('maxwidth');?>"
+        value="<?php echo !empty($maxwidth) ? $maxwidth : "500px"; ?>" />
+</p>
+<?php
+        
+    }
+    /* time ago function start */
+
+        public function timeAgo($timestamp){
+            $time = time() - $timestamp;
+            if ($time < 60)
+                return  ( $time > 1 ) ? $time . ' seconds' : 'a second';
+            elseif ($time < 3600) {
+                $tmp = floor($time / 60);
+                return ($tmp > 1) ? $tmp . ' minutes' : ' a minute';
+            }
+            elseif ($time < 86400) {
+                $tmp = floor($time / 3600);
+                return ($tmp > 1) ? $tmp . ' hours' : ' a hour';
+            }
+            elseif ($time < 2592000) {
+                $tmp = floor($time / 86400);
+                return ($tmp > 1) ? $tmp . ' days' : ' a day';
+            }
+            elseif ($time < 946080000) {
+                $tmp = floor($time / 2592000);
+                return ($tmp > 1) ? $tmp . ' months' : ' a month';
+            }
+            else {
+                $tmp = floor($time / 946080000);
+                return ($tmp > 1) ? $tmp . ' years' : ' a year';
+            }
+            }
+			
+	/* time ago functon end */
+    public function widget($args, $instance) {
+        extract($args);
+        extract($instance);
+        $title = apply_filters('widget_title', $title);
+        $description = apply_filters('widget_description', $description);
+        if(empty($title)) $title = "Google Plus Activity";
+        if(empty($userid)) $userid = "116899029375914044550";
+        if(empty($gplus_api)) $gplus_api = "AIzaSyCPtQ1XMWetDkVw5XuzNnVXpf8ezDtH9So";
+        if(empty($count)) $count = "5";
+        if(empty($maxwidth)) $maxwidth = "500px";
+        /* Start Google Plus Information */
+        $gprofile_url = "https://www.googleapis.com/plus/v1/people/$userid?key=$gplus_api";
+        $gprofile = json_decode(file_get_contents($gprofile_url));
+        /* End Google Plus Information */
+
+        /* Start Grabbing Google Plus Activity */
+        $gactivity_url = "https://www.googleapis.com/plus/v1/people/$userid/activities/public?maxResults=$count&key=$gplus_api";
+        $gactivity = json_decode(file_get_contents($gactivity_url));
+        /* End Grabbing Google Plus Activity */
+
+        echo $before_widget;
+        echo $before_title . $title . $after_title;
+        
+        ?>
+<div id="gplus-activity" style="max-width: <?php echo $maxwidth; ?>;">
+	<div class="gplus-user">
+		<a href="<?php echo $gprofile->url; ?>" target="_blank">
+                    <img src="<?php echo $gprofile->image->url; ?>" alt="<?php echo $gprofile->displayName; ?>" title="<?php echo $gprofile->displayName; ?>">
+		</a>
+		<div class="follow-text">Follow <a href="<?php echo $gprofile->url; ?>" target="_blank"><?php echo $gprofile->displayName; ?></a> on Google Plus</div>
+		<div class="gplus-tagline"><?php echo $gprofile->tagline; ?></div>
+	</div>
+	<?php
+	foreach($gactivity->items as $item):
+	?>
+	<div class="gplus-posts">
+		<div class="gplus-text">
+			<?php echo $item->title; ?>
+		</div>
+		<div class="gplus-bottom">
+			<div class="gplus-time">
+				posted <?php echo $this->timeAgo(strtotime($item->published)); ?> ago
+			</div>
+			<div class="gplus-readmore">
+				<a href="<?php echo $item->url; ?>" target="_blank">read more</a>
+			</div>
+		</div>
+	</div>
+	<?php endforeach; ?>
+</div>
+<?php
+    echo $after_widget;
+    }
+}
+//start registering the extension
+add_action('widgets_init','register_GooglePlusActivity');
+function register_GooglePlusActivity(){
+    register_widget('GooglePlusActivity');
+}
---
+++ readme.txt
@@ -0,0 +1,98 @@
+=== Google Plus Activity ===
+
+Plugin URI: http://bigbluegroup.net/
+Author Name : Big Blue Group
+Author URL : http://bigbluegroup.net/
+Tags: google plus activity, google plus activity feed, google plus activity feed widget, google plus widget, google plus custom feed, wordpress plugin for google plus activity
+Requires at least: 3.0.1
+Tested up to: 3.9.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Google Plus activity list all of the activities in the specified collection for a particular user.
+
+== Description ==
+
+Google Plus activity list all of the activities in the specified collection for a particular user.
+
+Simply it display feeds of any user but one thing to remember you can display public posts or feeds on your website only.
+
+Some remarkable feature of Google Plus Activity
+
+1) It's simple
+
+2) Very easy to use.
+
+3) Documentation Available with images
+
+4) Responsive
+
+5) Easily customizable. We have set classes everywhere to help you out.
+
+6) It uses Google Plus own API library - No third party library.
+
+7) 24/7 Supports Available
+
+== Installation ==
+
+Thanks for installing Google Plus Activity Plugin. This is a wordpress widget plugin.
+
+Configuration Fields:
+
+1. Google Plus User ID :-
+
+Numeric ID of your Google Plus Profile
+
+To find your google plus id follow the below steps.
+ 
+Go to your profile or the ones you like to display on sidebar
+ 
+https://plus.google.com/+google
+ 
+Something like this.
+ 
+Find one of their post and hover over their name or image using mouse.
+
+Right click the mouse and copy the link url
+
+https://plus.google.com/116899029375914044550
+
+Something like this URL will be copied (upone placed this on notepad).
+
+The numeric numbers are your Google Plus User ID.
+
+2. Google Plus API Key :-
+
+https://code.google.com/apis/console#access
+
+Create a New Project – give it a name and then save
+ 
+Now you have to Enable Google+ Api First. To do that first click on the project you have just created. Sign agreement if required or being asked. Now see the left menu set. Click on API & Auth – find Google+ Api and set it On
+ 
+Now from left click on Credentials – Click Button name “CREATE NEW KEY” – Select Browser key – give domain name if want – the domain name you will set the key will only accessible from that domain. 
+ 
+Copy the API Key and paste in to your Joomla module field name API Key
+
+3. Max Number of Google Post :-
+
+No of Max post you like to display
+
+4. Maximum Width :-
+
+Maximum width of your module in pixel.
+
+== Frequently Asked Questions ==
+
+Feel free to contact us anytime
+
+Contact Email :- bigbluegroup@outlook.com
+
+== Screenshots ==
+
+1. Frontend Screenshot
+2. Widget Backend
+
+== Changelog ==
+
+= 0.1 =
+Stable version release
